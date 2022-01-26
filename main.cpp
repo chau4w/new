4f58542c1183483a89e907d07195b0985292ef1b
Chouchou44 <91860520+chau4w@users.noreply.github.com>
2022-01-26 17:01:19 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,9 @@
 #include <iostream>
-#include<algorithm>
 
 using namespace std;
- bool check (string a, string b){
-    return (a+b)>(b+a);
- }
+
 int main()
 {
-    string a[100];
-    int n; cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-    sort(a,a+n,check);
-    for(int i=0;i<n;i++){
-        cout<<a[i];
-    }
+    cout << "Hello world!" << endl;
     return 0;
 }
